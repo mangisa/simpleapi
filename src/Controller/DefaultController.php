baa58b75cb32f36c48d1ab4920b2ec772c37a862
mangisa <mangisa@hotmail.es>
2021-07-12 18:00:24 +0200

    Consulting contry by api

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,21 +3,62 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="main")
      */
     public function index(): Response
     {
-        $myMail = $this->getParameter('app.admin_email');
+        $data = json_decode(file_get_contents('https://restcountries.eu/rest/v2/all?fields=name'), true);
         
+        $countryNames = array();
+        foreach ($data as $key => $arrayCountry) {
+            array_push($countryNames, $arrayCountry['name']);
+        }
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-            'my_mail' => $myMail,
+            'my_mail' => $this->getParameter('app.admin_email'),
+            'countries_names' => $countryNames,
         ]);
     }
+
+    /**
+     * @Route("/api_get/{country}", name="country_api_get", methods={"GET"})
+     */
+    public function countryApiGet($country): JsonResponse
+    {
+        $url = 'https://restcountries.eu/rest/v2/name/' . $country;
+        $data = json_decode(file_get_contents($url), true);
+
+        return new JsonResponse(
+            array(
+                'data' => reset($data),
+                'country' => $country,
+            ), 200
+        );
+    }
+
+    /**
+     * @Route("/db_get/{country}", name="country_db_get", methods={"GET"})
+     */
+    public function countryDBGet($country): JsonResponse
+    {
+        $url = 'https://restcountries.eu/rest/v2/name/' . $country;
+        $data = json_decode(file_get_contents($url), true);
+
+        return new JsonResponse(
+            array(
+                'data' => reset($data),
+                'country' => $country,
+            ), 200
+        );
+    }
+
+    
 }
--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +15,43 @@ class ClientType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('birthdate')
-            ->add('nie')
-            ->add('address')
-            ->add('postalcode')
-            ->add('town')
-            ->add('city')
-            ->add('province')
-            ->add('country')
+            ->add('name', TextType::class, [
+                'label'  => 'Name:',
+                'help' => 'Make sure to add a valid name',
+            ])
+            ->add('surname', TextType::class, [
+                'label'  => 'Surname:',
+                'help' => 'Make sure to add a valid surname',
+            ])
+            ->add('birthdate', DateType::class, [
+                'label' => 'Birth date:',
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'js-datepicker'
+                ],
+            ])
+            ->add('nie', TextType::class, [
+                'label'  => 'NIE:',
+                'help' => 'Make sure to add a valid nie',
+            ])
+            ->add('address', TextType::class, [
+                'label'  => 'Address:',
+            ])
+            ->add('postalcode', IntegerType::class, [
+                'label'  => 'Postal code:',
+            ])
+            ->add('town', TextType::class, [
+                'label'  => 'Town:',
+            ])
+            ->add('city', TextType::class, [
+                'label'  => 'City:',
+            ])
+            ->add('province', TextType::class, [
+                'label'  => 'Province:',
+            ])
+            ->add('country', TextType::class, [
+                'label'  => 'Country:',
+            ])
         ;
     }
 
